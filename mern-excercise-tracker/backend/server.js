ec3b882114815a0774c8ca4e0302b59ac472495e
roc-cyber <62799193+roc-cyber@users.noreply.github.com>
2021-05-07 13:10:52 +0530

    first commit

---
+++ mern-excercise-tracker/backend/server.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
+
+require('dotenv').config();
+
+const app=express();
+const port=process.env.PORT || 5000; //npm run server
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongoose to connect with mongodb
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercise');
+const usersRouter = require('./routes/user');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port,()=>{
+    console.log(`server is running on port:${port}`);
+});
